507a01a970cead6a3ee61e86d506b9764058415b
michaelmackenzie <michaelmackenzie@u.northwestern.edu>
2025-01-08 11:16:25 -0600

    Update CI data files, add all output to stdout, and update reference file

--- ci/check_trigger_results.sh
+++ ci/check_trigger_results.sh
@@ -2,12 +2,17 @@
 # Compare a local trigger log to a reference log file
 
 # Process the example data-like digi file
-NEVENTS=1000
+NEVENTS=-1
 DATAFILE="mu2e_trig_config/ci/data_files.txt"
 LOGFILE="local_trigger_results.log"
 REFERENCE="mu2e_trig_config/ci/reference_log_file.log"
 mu2e -c mu2e_trig_config/test/triggerTest.fcl -S ${DATAFILE} -n ${NEVENTS} >| ${LOGFILE}
 STATUS=$?
+
+# Add the log to the stdout report
+cat ${LOGFILE}
+
+# Check if the processing failed
 if [ $STATUS -ne 0 ]; then
     echo ">>> Failed to process triggerTest.fcl"
     exit 2
